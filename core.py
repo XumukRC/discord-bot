a7445a979a099b98f880c843ee547ec33da3e1e0
Haitaka <haitaka@ya.ru>
2016-01-10 18:15:14 +0000

    yield from instead of await

--- core.py
+++ core.py
@@ -16,12 +16,12 @@ def on_ready():
 @bot.command()
 @asyncio.coroutine
 def add(left : int, right : int):
-    await bot.say(left + right)
+    yield from bot.say(left + right)
 	
 @bot.command()
 @asyncio.coroutine
 def минус(left : int, right : int):
-    await bot.say(left - right)
+    yield from bot.say(left - right)
 
 @bot.command()
 @asyncio.coroutine
@@ -29,37 +29,37 @@ def roll(dice : str):
     try:
         rolls, limit = map(int, dice.split('d'))
     except Exception:
-        await bot.say('Format has to be in NdN!')
+        yield from bot.say('Format has to be in NdN!')
         return
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
+    yield from bot.say(result)
 
 @bot.command()
 @asyncio.coroutine
 def choose(*choices : str):
-    await bot.say(random.choice(choices))
+    yield from bot.say(random.choice(choices))
 
 @bot.command()
 @asyncio.coroutine
 def repeat(times : int, content='repeating...'):
     for i in range(times):
-        await bot.say(content)
+        yield from bot.say(content)
 
 @bot.command()
 @asyncio.coroutine
 def joined(member : discord.Member):
-    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+    yield from bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
 @bot.group(pass_context=True)
 @asyncio.coroutine
 def cool(ctx):
     if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+        yield from bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
 
 @cool.command()
 @asyncio.coroutine
 def bob():
-    await bot.say('Yes, bob is cool.')
+    yield from bot.say('Yes, bob is cool.')
 
 bot.run('haitaka@yandex.ru', '22:=Kp|/|cToF')
