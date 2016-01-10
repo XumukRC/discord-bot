cdf2bf010f78120974ee0c77ef24c180dc951d04
Haitaka <haitaka@ya.ru>
2016-01-10 18:14:09 +0000

    indent correction

--- core.py
+++ core.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix='?')
 
 @bot.event
 @asyncio.coroutine
- def on_ready():
+def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -15,17 +15,17 @@ bot = commands.Bot(command_prefix='?')
 
 @bot.command()
 @asyncio.coroutine
- def add(left : int, right : int):
+def add(left : int, right : int):
     await bot.say(left + right)
 	
 @bot.command()
 @asyncio.coroutine
- def минус(left : int, right : int):
+def минус(left : int, right : int):
     await bot.say(left - right)
 
 @bot.command()
 @asyncio.coroutine
- def roll(dice : str):
+def roll(dice : str):
     try:
         rolls, limit = map(int, dice.split('d'))
     except Exception:
@@ -37,29 +37,29 @@ bot = commands.Bot(command_prefix='?')
 
 @bot.command()
 @asyncio.coroutine
- def choose(*choices : str):
+def choose(*choices : str):
     await bot.say(random.choice(choices))
 
 @bot.command()
 @asyncio.coroutine
- def repeat(times : int, content='repeating...'):
+def repeat(times : int, content='repeating...'):
     for i in range(times):
         await bot.say(content)
 
 @bot.command()
 @asyncio.coroutine
- def joined(member : discord.Member):
+def joined(member : discord.Member):
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
 @bot.group(pass_context=True)
 @asyncio.coroutine
- def cool(ctx):
+def cool(ctx):
     if ctx.invoked_subcommand is None:
         await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
 
 @cool.command()
 @asyncio.coroutine
- def bob():
+def bob():
     await bot.say('Yes, bob is cool.')
 
 bot.run('haitaka@yandex.ru', '22:=Kp|/|cToF')
