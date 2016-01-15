01330642ff682e5d63fe12cb5cca36049c168ac5
Haitaka <haitaka@ya.ru>
2016-01-15 19:14:31 +0000

    prefetch removed

--- uploads.py
+++ uploads.py
@@ -11,12 +11,12 @@ async def on_message(message):
 	await bot.process_commands(message)
 	
 	if message.channel.name == "uploads_test" and message.attachments:
-		upload = requests.get(message.attachments.url, prefetch=False)
-		mime = magic.from_buffer(upload.iter_content(256).next(), mime=True)
+		upload = requests.get(message.attachments[0]["url"])
+		mime = magic.from_buffer(upload, mime=True)
 		if mime.startswith("audio"):
 			db_conn = sqlite3.connect('uploads.db')
 			db_cur = db_conn.cursor()
 			db_cur.execute("INSERT INTO songs (uploader, url) \
-							VALUES ({0.author.name}, {0.attachments.url})".format(message))
+							VALUES ({0.author.name}, {0.attachments[0]['url']})".format(message))
 			db_conn.commit()
 			db_conn.close()
