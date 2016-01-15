d5e8670671cf6a5dbd0909af19511381a3549dd8
Haitaka <haitaka@ya.ru>
2016-01-15 19:04:36 +0000

    uploads.py

--- uploads.py
+++ uploads.py
@@ -1,11 +1,22 @@
+import requests
+import magic
+import sqlite3
 import discord
 from connect import bot
 
+#CREATE TABLE songs (title text, artist text, uploader text, url text, date text)
+
 @bot.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == bot.user:
-        return
-
-    msg = 'Hello {0.attachments}'.format(message)
-    await bot.send_message(message.channel, msg)
+	await bot.process_commands(message)
+	
+	if message.channel.name == "uploads_test" and message.attachments:
+		upload = requests.get(message.attachments.url, prefetch=False)
+		mime = magic.from_buffer(upload.iter_content(256).next(), mime=True)
+		if mime.startswith("audio"):
+			db_conn = sqlite3.connect('uploads.db')
+			db_cur = db_conn.cursor()
+			db_cur.execute("INSERT INTO songs (uploader, url) \
+							VALUES ({0.author.name}, {0.attachments.url})".format(message))
+			db_conn.commit()
+			db_conn.close()
