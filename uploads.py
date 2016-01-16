14af23431aa60bc2464e0434a6d08a3948be04bc
Haitaka <haitaka@ya.ru>
2016-01-16 08:49:53 +0000

    uploads fix

--- uploads.py
+++ uploads.py
@@ -4,6 +4,18 @@ import sqlite3
 import discord
 from connect import bot
 
+db_conn = sqlite3.connect('uploads.db')
+db_cur = db_conn.cursor()
+db_cur.execute("""CREATE TABLE IF NOT EXISTS songs ( \
+				id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+				title text, \
+				artist text, \
+				uploader text, \
+				url text, \
+				timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)""")
+db_conn.commit()
+db_conn.close()
+
 @bot.event
 async def on_message(message):
 	await bot.process_commands(message)
@@ -12,15 +24,9 @@ async def on_message(message):
 		upload = requests.get(message.attachments[0]["url"])
 		mime = magic.from_buffer(upload.content, mime=True)
 		if mime.startswith(b"audio"):
+			bot.say("{0[0]['filename']} registred".format(message.attachments))
 			db_conn = sqlite3.connect('uploads.db')
 			db_cur = db_conn.cursor()
-			db_cur.execute("""CREATE TABLE IF NOT EXISTS 'songs' ( \
-							id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
-							title text, \
-							artist text, \
-							uploader text, \
-							url text, \
-							timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)""")
 			db_cur.execute("""INSERT INTO songs (uploader, url) \
 							VALUES ('{0.author.name}', '{1}')""".format(message, message.attachments[0]["url"]))
 			db_conn.commit()
