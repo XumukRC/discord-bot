b234c701bba1dcdbac7af0174dc05798c25d6249
Haitaka <haitaka@ya.ru>
2016-01-18 14:36:37 +0000

    random q from dir

--- music.py
+++ music.py
@@ -1,5 +1,8 @@
 import asyncio
 import sqlite3
+from os import listdir
+from os.path import isfile, join
+import random
 
 import discord
 from connect import bot
@@ -9,13 +12,16 @@ import uploads
 if not discord.opus.is_loaded():
 	discord.opus.load_opus('/usr/local/lib/libopus.so')
 
-class DJ:
+class Radio:
 	def __init__(self):
 		self.songs = asyncio.Queue()
 		self.play_next_song = asyncio.Event()
 		#self.starter = None
 		self.player = None
 		self.current = None
+		self.files_dir = "copyfs/radio/"
+		# list ONLY files
+		self.files = [f for f in listdir(self.files_dir) if isfile(join(self.files_dir, f))]
 
 	def toggle_next_song(self):
 		bot.loop.call_soon_threadsafe(self.play_next_song.set)
@@ -25,12 +31,17 @@ class DJ:
 
 	def is_playing(self):
 		return self.player is not None and self.player.is_playing()
+		
+	async def random_q(self):
+		q = random.sample(self.files, len(self.files))
+		for song in q:
+			await self.songs.put(self.files_dir + song)
 
-bot_dj = DJ()
+radio = Radio()
 		
 @bot.command(pass_context=True)
 async def join(context, channel_name : str):
-	global bot_dj
+	global radio
 	if bot.is_voice_connected():
 		await bot.say('Already connected to a voice channel')
 		
@@ -41,62 +52,58 @@ async def join(context, channel_name : str):
 		await bot.say('Cannot find a voice channel by that name.')
 
 	await bot.join_voice_channel(channel)
-	#bot_dj.starter = context.message.author
+	#radio.starter = context.message.author
 	
 @bot.command()
 async def leave():
-	global bot_dj
-	#if not bot_dj.can_control_song(message.author):
+	global radio
+	#if not radio.can_control_song(message.author):
 	#	return
 	#self.starter = None
 	await bot.voice.disconnect()
 
 @bot.command()
 async def pause():
-	global bot_dj
+	global radio
 	#if not self.can_control_song(message.author):
 	#	fmt = 'Only the requester ({0.current.requester}) can control this song'
 	#	await self.send_message(message.channel, fmt.format(self))
-	if bot_dj.player.is_playing():
-		bot_dj.player.pause()
+	if radio.player.is_playing():
+		radio.player.pause()
 
 @bot.command()
 async def resume():
-	global bot_dj
+	global radio
 	#if not self.can_control_song(message.author):
 	#	fmt = 'Only the requester ({0.current.requester}) can control this song'
 	#	await self.send_message(message.channel, fmt.format(self))
-	if bot_dj.player is not None and not bot_dj.is_playing():
-		bot_dj.player.resume()
+	if radio.player is not None and not radio.is_playing():
+		radio.player.resume()
 
 @bot.command()
 async def skip():
-	if bot_dj.player.is_playing():
-		bot_dj.player.stop()
-		bot_dj.toggle_next_song()
+	if radio.player.is_playing():
+		radio.player.stop()
+		radio.toggle_next_song()
 
-@bot.command()
-async def play():
-	global bot_dj
-	if bot_dj.player is not None and bot_dj.player.is_playing():
+@bot.group(pass_context=True)
+async def play(ctx):
+	global radio
+	if radio.player is not None and radio.player.is_playing():
 		await bot.say('Already playing a song')
 		return
-	if bot_dj.songs.empty():
-		db_conn = sqlite3.connect('uploads.db')
-		db_cur = db_conn.cursor()
-		for song in db_cur.execute("""SELECT id, url FROM songs ORDER BY RANDOM()"""):
-			await bot_dj.songs.put(song[1])
-		db_conn.close()
+	if radio.songs.empty() and ctx.invoked_subcommand is None:
+		await radio.random_q()
 	while True:
 		if not bot.is_voice_connected():
 			await bot.say('Not connected to a voice channel')
 			return
 
-		bot_dj.play_next_song.clear()
-		bot_dj.current = await bot_dj.songs.get()
-		bot_dj.player = bot.voice.create_ffmpeg_player(bot_dj.current, after=bot_dj.toggle_next_song)
-		bot_dj.player.start()
+		radio.play_next_song.clear()
+		radio.current = await radio.songs.get()
+		radio.player = bot.voice.create_ffmpeg_player(radio.current, after=radio.toggle_next_song)
+		radio.player.start()
 		fmt = 'Playing song "{0}"'
-		await bot.say(fmt.format(bot_dj.current))
-		await bot_dj.play_next_song.wait()
+		await bot.say(fmt.format(radio.current))
+		await radio.play_next_song.wait()
 	
