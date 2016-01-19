13ae02051c121e3fff48e6231d911cf54d901131
Haitaka <haitaka@ya.ru>
2016-01-19 13:37:50 +0000

    another fix

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings:
 		login  = db_cur.execute("SELECT value FROM settings WHERE setting='bot_login'").fetchone()[0]
 		passwd = db_cur.execute("SELECT value FROM settings WHERE setting='bot_passwd'").fetchone()[0]
 		db_conn.close()
-		return {'login': login, 'passwd': pqsswd}
+		return {'login': login, 'passwd': passwd}
 		
 	def copy_auth(self):
 		db_conn = sqlite3.connect(self.database)
@@ -19,7 +19,7 @@ class Settings:
 		login  = db_cur.execute("SELECT value FROM settings WHERE setting='copy_login'").fetchone()[0]
 		passwd = db_cur.execute("SELECT value FROM settings WHERE setting='copy_passwd'").fetchone()[0]
 		db_conn.close()
-		return {'login': login, 'passwd': pqsswd}
+		return {'login': login, 'passwd': passwd}
 		
 settings = Settings()
 		
