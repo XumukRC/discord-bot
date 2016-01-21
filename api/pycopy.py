a87ad6eb2348106950ae9252b3f9add9dedb23e5
Haitaka <haitaka@ya.ru>
2016-01-21 08:20:31 +0000

    indent fix

--- api/pycopy.py
+++ api/pycopy.py
@@ -56,10 +56,10 @@ class Copy(object):
 		local_filename = "tmp_uploads/" * url.split('/')[-1]
 		r = requests.get(url, stream=True)
 		with open(local_filename, 'wb') as f:
-		for chunk in r.iter_content(chunk_size=1024): 
-			if chunk: # filter out keep-alive new chunks
-				f.write(chunk)
-				#f.flush() #commented by recommendation from J.F.Sebastian
+			for chunk in r.iter_content(chunk_size=1024): 
+				if chunk: # filter out keep-alive new chunks
+					f.write(chunk)
+					#f.flush() #commented by recommendation from J.F.Sebastian
 	return local_filename
 	
 	def get_headers_str(self):
